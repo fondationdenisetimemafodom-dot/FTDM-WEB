fc18b1723ac9a39106b2c6b900866833e710633e
Enowtabi <enowbibi@gmail.com>
2025-10-15 00:09:06 +0100

    [fix](deployment):fix env variables

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // src/lib/api.ts
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "https://fdtm-api.onrender.com";
 
 export default API_BASE_URL;
--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -7,7 +7,7 @@ import type {
 } from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "https://fdtm-api.onrender.com";
 
 // Create axios instance
 const axiosInstance = axios.create({
