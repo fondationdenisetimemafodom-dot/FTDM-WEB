622263bbc9fe0253ea8314579fe0c21c54c4f1c8
ONYA237 <onyamartha@gmail.com>
2025-12-12 12:07:56 +0100

    swiper modules declared

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -24,9 +24,7 @@ import { Loader2 } from "lucide-react";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import 'swiper/css/bundle';
 type MediaItem = {
   publicId: string;
   type: string;
---
+++ src/swiper.d.ts
@@ -0,0 +1,18 @@
+// Swiper CSS type declarations for TypeScript
+declare module 'swiper/css' {
+  const content: string;
+  export default content;
+}
+declare module 'swiper/css/bundle' {
+  const content: string;
+  export default content;
+}
+declare module 'swiper/css/navigation' {
+  const content: string;
+  export default content;
+}
+declare module 'swiper/css/pagination' {
+  const content: string;
+  export default content;
+}
+// Add more if you use other modules like 'swiper/css/autoplay'
